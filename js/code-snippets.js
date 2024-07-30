e7c692866443d20e4e13643fb537e16af66f998e
Max <lythgoemax@gmail.com>
2024-07-30 16:52:52 -0600

    minor styling changes

--- js/code-snippets.js
+++ js/code-snippets.js
@@ -14,5 +14,61 @@ export const codeSnippets = [
     <?php endwhile; ?>
     </div>
 <?php endif; ?>`
+    },
+    {
+        id: 2,
+        title: 'Escaping HTML ACF',
+        isFavorite: true,
+        codeCopy: 
+        `// Allow script tags in ACF subfields
+add_filter('wp_kses_allowed_html', 'acf_allow_script_tags', 10, 2);
+function acf_allow_script_tags($tags, $context) {
+    if ($context === 'acf') {
+        $tags['script'] = array(
+            'src' => true,
+            'type' => true,
+            'async' => true,
+            'defer' => true,
+            'nonce' => true,
+            'language' => true,
+            'charset' => true,
+            'crossorigin' => true,
+            'integrity' => true,
+            'nomodule' => true,
+            'onload' => true,
+            'onerror' => true,
+            'nonce' => true,
+            'nonce' => true,
+            'nonce' => true,
+            // Add any other script-related attributes you need
+        );
+    }
+
+    return $tags;
+}
+
+// Fix ACF escaping HTML (iframes)
+add_filter( 'wp_kses_allowed_html', 'acf_add_allowed_iframe_tag', 10, 2 );
+function acf_add_allowed_iframe_tag( $tags, $context ) {
+    if ( $context === 'acf' ) {
+        $tags['iframe'] = array(
+            'src'             => true,
+            'height'          => true,
+            'width'           => true,
+            'frameborder'     => true,
+            'allowfullscreen' => true,
+        );
+    }
+
+    return $tags;
+}
+`
+    }, 
+    {
+        id: 3,
+        title: 'PHP Shortcode',
+        isFavorite: true,
+        codeCopy: 
+        `<?php echo do_shortcode('[hero-title]'); ?>`
     }
 ]
